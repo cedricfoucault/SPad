6a0fc79999e38992484efadba4b6eaf6d8ce079f
Cédric Foucault <cedric.foucault@gmail.com>
2013-12-08 21:47:39 +0100

    Initial Commit

---
+++ SPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SPad
+//
+//  Created by Cédric Foucault on 08/12/13.
+//  Copyright (c) 2013 Cédric Foucault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SPadTests/SPadTests.m
@@ -0,0 +1,34 @@
+//
+//  SPadTests.m
+//  SPadTests
+//
+//  Created by Cédric Foucault on 08/12/13.
+//  Copyright (c) 2013 Cédric Foucault. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SPadTests : XCTestCase
+
+@end
+
+@implementation SPadTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
